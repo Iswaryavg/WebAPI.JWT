ded6d6edbb5ef3f597b9b3f9341db4e45ee51aa8
Iswaryavg <61296893+Iswaryavg@users.noreply.github.com>
2021-01-25 18:59:25 +0000

    Add project files.

---
+++ WebAPI.JWT/Controllers/IJWTAuthenticationManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.JWT.Controllers
+{
+    public interface IJWTAuthenticationManager
+    {
+        public string Authenticate(string username, string password);
+    }
+}
---
+++ WebAPI.JWT/Controllers/NameController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.JWT.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly IJWTAuthenticationManager jWTAuthenticationManager;
+
+        public NameController(IJWTAuthenticationManager jWTAuthenticationManager)
+        {
+            this.jWTAuthenticationManager = jWTAuthenticationManager;
+     
+        }
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        [HttpGet("{id}",Name ="Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token = jWTAuthenticationManager.Authenticate(userCred.UserName, userCred.Password);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(token);
+        }
+    }
+}
